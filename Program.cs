fc62338c58f95d65766b4e0ae82a742ccc68ed4b
dyogocae <dyogocae@gmail.com>
2023-10-06 15:27:18 -0300

    TODOs finalizados

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "123456", modelo: "nokia1", imei: "1111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Gmail");
+
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Iphone 13", imei: "1313", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Gmail");
